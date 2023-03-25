78401d1698bcfbdc62c26e1754837061b81e79b9
yansem <janchevsem@mail.ru>
2023-03-25 12:44:12 +0300

    edit database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,62 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(500)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'first_name' => 'Erland',
+            'last_name' => 'Muchasaj',
+            'company' => 'EMCMS Corporation',
+            'email' => 'test@example.com',
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'Erland',
+            'last_name' => 'Muchasaj',
+            'company' => 'EMCMS Group',
+            'email' => 'test@emcms.com',
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'Erland',
+            'last_name' => 'Doe',
+            'company' => 'EMCMS LTD',
+            'email' => 'info@doe.com',
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'Erland',
+            'last_name' => 'Doe',
+            'company' => 'EMCMS Corporation',
+            'email' => 'info@erland.com',
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'john',
+            'last_name' => 'Muchasaj',
+            'company' => 'Pimlico LTD',
+            'email' => 'info@miltons.com',
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'john',
+            'last_name' => 'Doe',
+            'company' => 'Pimlico LTD',
+            'email' => 'info@mjohn.com',
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'Bill',
+            'last_name' => 'Gates',
+            'company' => 'Microsoft Corporation',
+            'email' => 'bill.gates@microsoft.com',
+        ]);
+
+        User::factory()->create([
+            'first_name' => 'Tim',
+            'last_name' => 'O\'Reilly',
+            'company' => 'O\'Reilly Media Inc.',
+            'email' => 'tim@oreilly.com',
+        ]);
     }
 }
